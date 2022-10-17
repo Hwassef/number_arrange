461422b12abf08c4b97f7692500cff7830449c97
Hwassef <hassine.wassef@outloook.com>
2022-10-17 00:59:43 -1100

    Holds different const padding values

---
+++ lib/config/paddings/app_paddings.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppPaddings {
+  /// Private constructor
+  AppPaddings._();
+  static const double regularPaddingValue = 16.0;
+  static const EdgeInsets horizontalRegularPaddingValue = EdgeInsets.symmetric(horizontal: regularPaddingValue);
+}
