bd11dfac0df798af083ad309f7f27a55b7804243
Hwassef <hassine.wassef@outloook.com>
2022-10-17 01:49:02 -1100

    Initialize default elevated button theme

--- lib/config/theme/app_default_theme.dart
+++ lib/config/theme/app_default_theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:number_arrange/config/theme/app_elevated_button_theme.dart';
 import 'package:number_arrange/config/theme/input_decoration_theme.dart';
 
 class AppDefaultTheme {
   AppDefaultTheme._();
   static ThemeData defaultThemeData = ThemeData(
     inputDecorationTheme: AppInputDecorationTheme.inputDecorationTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.elevatedButtonThemeDataLight,
   );
 }
