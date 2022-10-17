09b03741d0242205d4dde3b2c168924a20dffea7
Hwassef <hassine.wassef@outloook.com>
2022-10-17 01:13:02 -1100

    Ad dark blue color const

--- lib/config/colors/app_colors.dart
+++ lib/config/colors/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color redColor = Color(0xFFFF0000);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color greyColor = Color(0xFF808080);
+  static const Color darkBlueColor = Color(0xFF00008B);
 }
