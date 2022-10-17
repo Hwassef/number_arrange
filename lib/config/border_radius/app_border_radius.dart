194ec30b1fe1b58f728eb4ffcf251e1f1ee4813d
Hwassef <hassine.wassef@outloook.com>
2022-10-17 01:10:20 -1100

    Holds border radius const values

---
+++ lib/config/border_radius/app_border_radius.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppBorderRadius {
+  AppBorderRadius._();
+  static const double normalRadiusValue = 6.0;
+  static const Radius normalRadius = Radius.circular(normalRadiusValue);
+  static const BorderRadius normalBorderRadius = BorderRadius.all(normalRadius);
+}
