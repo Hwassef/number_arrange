0e9f775817ae7bc7dc410825e687dd3a214026df
Hwassef <hassine.wassef@outloook.com>
2022-10-17 01:06:47 -1100

    Add white color const

--- lib/config/colors/app_colors.dart
+++ lib/config/colors/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
   static const Color redColor = Color(0xFFFF0000);
+  static const Color whiteColor = Color(0xFFFFFFFF);
 }
