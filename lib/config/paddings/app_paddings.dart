ce58d31910ee73908c3f766a93f43b5b390a5e55
Hwassef <hassine.wassef@outloook.com>
2022-10-17 01:15:30 -1100

    Add input content padding value

--- lib/config/paddings/app_paddings.dart
+++ lib/config/paddings/app_paddings.dart
@@ -3,6 +3,12 @@ import 'package:flutter/cupertino.dart';
 class AppPaddings {
   /// Private constructor
   AppPaddings._();
+  static const double smallPaddingValue = 6.0;
   static const double regularPaddingValue = 16.0;
+  static const double largePadding = 22.0;
   static const EdgeInsets horizontalRegularPaddingValue = EdgeInsets.symmetric(horizontal: regularPaddingValue);
+  static const EdgeInsets inputContentPaddingPadding = EdgeInsets.symmetric(
+    horizontal: smallPaddingValue,
+    vertical: largePadding,
+  );
 }
