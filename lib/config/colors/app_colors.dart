ee75f5356b463c534450861a246c1f32907c2cc8
Hwassef <hassine.wassef@outloook.com>
2022-10-17 01:08:03 -1100

    Add grey color const

--- lib/config/colors/app_colors.dart
+++ lib/config/colors/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   AppColors._();
   static const Color redColor = Color(0xFFFF0000);
   static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greyColor = Color(0xFF808080);
 }
